73a68ba40f59a45e3713cfdfc00b6047431b207e
Justin Wolf <jwolf@keansburg.k12.nj.us>
2015-06-12 10:12:33 -0400

    DB Migration: Added Default Status
    Added Status “Active” to db migration for initial values.

---
+++ inventory/migrations/0009_auto_20150612_1404.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+def load_defaults(apps, schema_editor):
+    Status = apps.get_model("inventory", "Status")
+    default_status = Status(id=1,name="Active", checkedout=True)
+    default_status.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inventory', '0008_status_checkedout'),
+    ]
+
+    operations = [
+    	migrations.RunPython(load_defaults)
+    ]
